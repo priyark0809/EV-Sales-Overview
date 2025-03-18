a117f46c19b2db0b721e9fe80a48016abffaa779
priyark0809 <150041007+priyark0809@users.noreply.github.com>
2025-03-18 19:15:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for,gunicorn
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3 as sql
 import sqlite3
